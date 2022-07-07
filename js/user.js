53c2e15c358cccd67b911904c52ccb395a28e4a1
jrlopez185 <79105416+jrlopez185@users.noreply.github.com>
2022-07-07 15:31:23 -0600

    Add files via upload

---
+++ js/user.js
@@ -0,0 +1,76 @@
+let user = [
+
+    {
+        id:100, 
+        nikname:'Anna0', 
+        password:'white00',
+        Name:'Ana Addison',
+        adress:'1325 Candy Rd, San Francisco, CA 96123',
+        email:'anna.addison@yahoo.com',
+        phone: 3841019535 },
+
+    {
+        id:101, 
+        nikname:'carol1', 
+        password:'orange01',
+        Name:'Carol Campbell',
+        adress:'1931 Brown St, Gainesville, FL 85321',
+        email:'anna.addison@yahoo.com',
+        phone: 2548952651 },
+
+    {
+        id:102, 
+        nikname:'julia2', 
+        password:'yellow02',
+        Name:'Julia Jones',
+        adress:'1622 Seaside St, Seattle, WA 32569',
+        email:'jolie.jones@msn.com',
+        phone: 3651469235 },
+
+
+    {
+        id:103, 
+        nikname:'irene3', 
+        password:'blue03',
+        Name:'Irene Everly',
+        adress:'1756 East Dr, Houston, TX 28562',
+        email:'irene.everly@gmail.com',
+        phone: 9859423698 },
+            
+    {
+        id:104, 
+        nikname:'rachel4', 
+        password:'red04',
+        Name:'Rachel Rose',
+        adress:'1465 River Dr, Boston, MA 436251',
+        email:'rachel.rose@hotmail.com',
+        phone: 2945632543 },
+
+    {
+        id:105, 
+        nikname:'sophie5', 
+        password:'green05',
+        Name:'Sophie Sutton',
+        adress:'1896 West Dr, Portland, OR 65842',
+        email:'sophie.sutton@yahoo.com',
+        phone: 5169525614 },
+
+
+    {
+        id:106, 
+        nikname:'wendy6', 
+        password:'purple06',
+        Name:'Wendy West',
+        adress:'1252 Vine St, Chicago, IL 73215',
+        email:'wendy.west@gmail.com',
+        phone: 6645936156 },
+
+    {
+        id:107, 
+        nikname:'jefuentes', 
+        password:'gatitos123',
+        Name:'Jossue Fuentes',
+        adress:'Colegio Santa Catalina SCL',
+        email:'jefuentes@scl.edu.gt',
+        phone: 4568443548 }
+]
